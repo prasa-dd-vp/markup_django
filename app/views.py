282ebf735772389224380b1e3c22dd98b239cce9
Venkatesh Prasad <prasad.venkatesh90@gmail.com>
2018-08-09 18:09:37 +0530

    update view.py
    changed bullets and numbers function

--- app/views.py
+++ app/views.py
@@ -2,50 +2,65 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.http import HttpResponse
 import re
+
 # Create your views here.
-
-
-
 def home(request):
     return render(request, 'app/index.html')
 
 def bold(data):
-    #print("inside bold"+data)
-    return re.sub(r'\*\*([_/a-zA-Z0-9\\__,.\s]+)\*\*',r'<b>\1</b>',data)
+    print("inside bold: "+data)
+    return re.sub(r'\*\*([\*\w\n\s.]*)\*\*',r'<b>\1</b>',data) #_/a-zA-Z0-9\\__,.\s
 
 def italics(data):
     #print("inside italics"+data)
-    return re.sub(r'//([<a-z>]*)([^:][a-zA-Z\_,.\s]+)([/<a-z>]*)//',r'<i>\1\2\3</i>',data)
+    return re.sub(r'//([<a-z>]*)([^http:][<>/\w\n]*)([/<a-z>]*)//',r'<i>\1\2\3</i>',data)
 
 def underline(data):
     #print("inside underline"+data)
-    return re.sub(r'__([<a-z>]*)([a-zA-Z\\__,.\s]+)([/<a-z>]*)__',r'<u>\1\2\3</u>',data)
+    return re.sub(r'__([<a-z>]*)(.*[\w\n]*)([/<a-z>]*)__',r'<u>\1\2\3</u>',data)
 
 def strike(data):
     #print("inside underline"+data)
-    return re.sub(r'\$\$([<a-z>]*)([a-zA-Z\\__,.\s]+)([/<a-z>]*)\$\$',r'<strike>\1\2\3</strike>',data)
+    return re.sub(r'\$\$([<a-z>]*)(.*[\w\n]*)([/<a-z>]*)\$\$',r'<strike>\1\2\3</strike>',data)
 
 def newline(data):
-    return re.sub(r'([\n]+)',r'\1<br>',data) 
+    return re.sub(r'([\n])',r'<br>\1',data) 
+
 
 def bullets(data):
     #print("inside bullets "+data)
-    b = re.sub(r'\s{1}\*{1}\s{1}(.*)\n',r'<ul><li>\1</li></ul>',data)
-    b = b.replace("</ul><br><ul>","")
+    b = re.sub(r'^[^a-zA-Z0-9]*\*{1}\s{1}(.*)\n',r'<ul><li>\1</li></ul>\n',data, flags = re.M)
+    b = b.replace("</ul>\n<ul>","")
     b = b.replace("</ul><br>","</ul>")
+    print("inside bullets: "+b)
     return b
+
+
 
 def numbers(data):
     #print("inside fun"+ data)
-    n = re.sub(r'\s{1}[0-9]+.{1}\s{1}(.*)\n',r'<ol><li>\1</li></ol>',data)
+    n = re.sub(r'^[^a-zA-Z0-9]*[0-9]+.{1}\s{1}(.*)\n',r'<ol><li>\1</li></ol>',data, flags = re.M)
     #print(n)
-    n = n.replace("</ol><br><ol>","")
+    n = n.replace("</ol><ol>","")
     n = n.replace("</ol><br>","</ol>")
     return n
 
 def link(data):
     return re.sub(r'\[(\w+)\]\(([\w:/.]+)\)',r'<a href= "\2">\1</a>',data)
-    
+
+def h1_header(data):
+    return re.sub(r'[^#](#{1})\s(.*)\n',r'<h1>\2</h1>',data)
+
+def h2_header(data):
+    return re.sub(r'(#{2})\s(.*)\n',r'<h2>\2</h2>',data)
+
+def h3_header(data):
+    return re.sub(r'(#{3})\s(.*)\n',r'<h3>\2</h3>',data)
+
+
+
+def space(data):
+    return data.replace(" ", "&nbsp;")
 
 #re.sub(r'(\s{1}\*{1}\s{1}.*\n)+',r'<ol>\g<0></ol>',data)
 #((\s\*\s)+\s*\w*\n)+
@@ -63,7 +78,11 @@ def process(request):
         #print("after strike"+result)
         result = newline(result)
         result = bullets(result)
-        result = numbers(result)
+        result = numbers(result)   
+        #result = h1_header(result)
+        #result = h2_header(result)
+        #result = h3_header(result)
+        result = space(result)
         result = link(result)
         print(result)
         return HttpResponse(result)
